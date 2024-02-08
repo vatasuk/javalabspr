6eeff6a7c754a5536cb719d6a15a5faf3d887fbb
vatasuk <vatasuk111@gmail.com>
2024-02-08 18:24:12 +0300

    Initial commit

---
+++ src/main/java/com/example/javalabspr/JavalabsprApplication.java
@@ -0,0 +1,13 @@
+package com.example.javalabspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalabsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalabsprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javalabspr/JavalabsprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javalabspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavalabsprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
